6159ab820a92c60172eb4243bc83c87d6d2d7783
UpskillKenya <admin@upskillkenya.co.ke>
2025-03-04 20:45:37 +0300

    Remove middleware file for next-auth integration

--- src/middleware.ts
+++
@@ -1,7 +0,0 @@
-
-// apply next-auth to the whole application-means protected
-export {default} from 'next-auth/middleware'
-
-
-// or apply it to specific routes
-export const config = {matcher: ['/user/dashboard']}

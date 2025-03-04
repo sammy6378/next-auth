6f8c970a02d40a84ee91aa54cec2e8da15194442
sammy6378 <samuel202mwangi@gmail.com>
2025-03-04 10:07:25 +0300

    Add authentication context provider for access and activation tokens

---
+++ src/provider/authProvider.tsx
@@ -0,0 +1,33 @@
+
+"use client"
+
+import {createContext,Dispatch,SetStateAction,useContext,useState} from "react";
+import { SessionProvider } from "next-auth/react"
+
+interface IContext {
+  accessToken: string | null;
+  setAccessToken: Dispatch<SetStateAction<string | null>>;
+  activationToken: string | null;
+  setActivationToken: Dispatch<SetStateAction<string | null>>;
+}
+
+export const AppContext = createContext<IContext | undefined>(undefined);
+
+export default function ProviderFunction({children}: {children: React.ReactNode}) {
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [activationToken, setActivationToken] = useState<string | null>(null);
+
+  return (
+    <AppContext.Provider value={{accessToken,setAccessToken,activationToken,setActivationToken}}>
+        <SessionProvider>
+          {children}
+        </SessionProvider>
+    </AppContext.Provider>
+  );
+}
+
+export const useContextFunc = () => {
+  const context = useContext(AppContext);
+  if (!context) throw new Error("Context not found");
+  return context;
+};

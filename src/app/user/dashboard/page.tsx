03e466d18eecd4ad9db9d70c205d0b05adeeefa8
UpskillKenya <admin@upskillkenya.co.ke>
2025-03-06 08:59:19 +0300

    Refactor Dashboard component to simplify user session handling and improve loading state management

--- src/app/user/dashboard/page.tsx
+++ src/app/user/dashboard/page.tsx
@@ -1,39 +1,20 @@
-"use client";
+"use client"
 
-import { useSession } from "next-auth/react";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { useAuthUser } from "@/componets/utils/reduxStore";
+import { useAuthUser } from "@/componets/hooks/reduxStore";
 
 const Dashboard = () => {
-  const { data: session, status } = useSession(); // NextAuth session
-  const router = useRouter();
   
   const user = useAuthUser(); // ✅ Get user from Redux
-  const name = user?.name || "Authenticated User";
+  const name = user?.name;
 
 
-
-  useEffect(() => {
-    // Redirect if NO Redux user and NO NextAuth session
-    if (!user && !session && status !== "loading") {
-      router.push("/user/login");
-    }
-  }, [user, session, status, router]);
-
-  if (status === "loading") return <p>Loading...</p>;
+  if (!user) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
-      {session ? (
-        <h1>Welcome, {session.user?.name || "GitHub/Google User"}!</h1>
-      ) : user ? (
-        <>
-        <h1>Welcome, {name || "Authenticated User"}!</h1>
-        </>
-      ) : (
-        <p>Redirecting...</p>
-      )}
+        <h2>Welcome {name}!</h2>
     </div>
   );
 };

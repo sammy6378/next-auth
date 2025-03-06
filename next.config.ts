23ebd6a3a8435e464a73ec26ed7a9933d705d093
UpskillKenya <admin@upskillkenya.co.ke>
2025-03-06 09:02:47 +0300

    Update Next.js configuration to support remote image patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: ""
+      }
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;

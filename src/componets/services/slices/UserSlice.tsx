d1f0eca1ab0a7de6986426b767e7ff4295e8bf13
UpskillKenya <admin@upskillkenya.co.ke>
2025-03-06 09:01:03 +0300

    Refactor UserSlice to use TUser type and remove refreshToken handling

--- src/componets/services/slices/UserSlice.tsx
+++ src/componets/services/slices/UserSlice.tsx
@@ -1,20 +1,14 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
-export interface IUser {
-    name: string;
-    role: string;
-};
+import { TUser } from '../authService';
 
 interface AuthState {
     accessToken: string | null;
-    refreshToken: string | null;
     isAuthenticated: boolean;
-    user: IUser | null;
+    user: TUser | null;
 }
 
 const initialState: AuthState = {
     accessToken: null,
-    refreshToken: null,
     isAuthenticated: false,
     user: null
 };
@@ -23,26 +17,21 @@ const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
-        login(state, action: PayloadAction<{ user: IUser | null; accessToken: string; refreshToken: string }>) {
+        login(state, action: PayloadAction<{ user: TUser | null; accessToken: string; }>) {
             state.user = action.payload.user;
             state.accessToken = action.payload.accessToken;
-            state.refreshToken = action.payload.refreshToken;
             state.isAuthenticated = true;
         },
         logout(state) {
             state.user = null;
             state.accessToken = null;
-            state.refreshToken = null;
             state.isAuthenticated = false;
         },
         setAccessToken: (state, action: PayloadAction<string>) => {
             state.accessToken = action.payload;
         },
-        setRefreshToken: (state, action: PayloadAction<string>) => {
-            state.refreshToken = action.payload;
-        },
     },
 });
 
-export const { login, logout, setAccessToken, setRefreshToken } = authSlice.actions;
+export const { login, logout, setAccessToken } = authSlice.actions;
 export default authSlice.reducer;

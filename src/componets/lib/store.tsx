065a0ad8d8b47c42d2a27a52c7730f05b270c9de
UpskillKenya <admin@upskillkenya.co.ke>
2025-03-07 14:20:16 +0300

    Add whitelist to persistAuthConfig for accessToken and expiresAt

--- src/componets/lib/store.tsx
+++ src/componets/lib/store.tsx
@@ -8,7 +8,8 @@ import { authService } from "../services/authService";
 // persit auth config for auth
 const persistAuthConfig = {
     key: "Auth",
-    storage
+    storage,
+    whitelist: ["accessToken", "expiresAt"], 
 }
 
 // apply persistreducer to authslice

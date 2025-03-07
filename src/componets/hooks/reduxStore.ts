047f2001fe8de329ee29d6837ab6e9f78c8f0ff7
UpskillKenya <admin@upskillkenya.co.ke>
2025-03-07 14:19:26 +0300

    Add useAccessToken hook to retrieve access token from auth state

--- src/componets/hooks/reduxStore.ts
+++ src/componets/hooks/reduxStore.ts
@@ -2,9 +2,18 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/componets/lib/store";
 import { TUser } from "../services/authService";
 
+// user data
 export const useAuthUser = () => {
   const authState = useSelector((state: RootState) => state.auth);
   const user = authState.user as TUser | null;
   if(user)
   return user;
+};
+
+
+// accesstoken
+export const useAccessToken = () => {
+  const authState = useSelector((state: RootState) => state.auth.accessToken);
+  const accessToken = authState as string | null;
+  return { accessToken };
 };

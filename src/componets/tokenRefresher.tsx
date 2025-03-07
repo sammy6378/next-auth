51649f8a9cb39a2a56ebd5fafc14b1f2abb38ea1
UpskillKenya <admin@upskillkenya.co.ke>
2025-03-07 14:21:06 +0300

    Add jwt-decode package and remove unused TokenRefresher component

--- src/componets/tokenRefresher.tsx
+++
@@ -1,9 +0,0 @@
-"use client"; // ✅ Ensure this is a client component
-
-import { useTokenRefresh } from "./hooks/useRefreshToken";
-
-export default function TokenRefresher() {
-  useTokenRefresh(); // ✅ This runs only on the client
-
-  return null; // No UI, just runs in the background
-}
